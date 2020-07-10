97287f29859c92cbc0ad0adae97360f658609c8e
jenrie01 <32349824+jenrie01@users.noreply.github.com>
2020-07-10 14:10:05 +0800

    Add files via upload

---
+++ modules_in_python/main.py
@@ -0,0 +1,15 @@
+import utility
+# from utility import multiply, divide
+from shopping import shopping_cart
+# import shopping.shopping_cart
+# or
+# from shopping.shopping_cart import buy
+
+print(utility.multiply(3, 2))
+# print(multiply(3, 2))
+print(shopping_cart.buy('apple'))
+# print(shopping.shopping_cart.buy('apple'))
+# print(buy('apple'))
+
+# if __name__ == "__main__":
+#     pass
---
+++ modules_in_python/test.py
@@ -0,0 +1,12 @@
+# import random
+# from random import shuffle
+# import random as toavoidnamecollision
+from random import shuffle
+
+# print(random.random())
+# print(random.randint(1, 200))
+# print(random.choice([1, 2, 3, 4, 5]))
+
+my_list = [1, 2, 3, 4, 5]
+shuffle(my_list)
+print(my_list)
